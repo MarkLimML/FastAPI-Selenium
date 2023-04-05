205d731ecc305df4a64037a5618ccdb9c69bd116
MarkLimML <marklouislim@gmail.com>
2023-04-05 17:45:42 +0800

    .

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, BackgroundTasks, HTTPException
+from fastapi import FastAPI, BackgroundTasks, Request, HTTPException
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 from extract import *
 import os
-from flask import Flask, request, render_template
 from app import app
 
 SECRET = os.getenv("SECRET")
@@ -14,18 +14,36 @@ class Msg(BaseModel):
     msg: str
     secret: str
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
-async def root():
-    if request.method == "POST":
-        text = request.form['urltext']
-        print(text)
-        return render_template('results.html', title="TEST RESULT", urltext=text)
-    return render_template('form.html', title='TEST URL')
+@app.get('/', response_class=HTMLResponse)
+@app.get('/index', response_class=HTMLResponse)
+async def root(request: Request):
+    return """
+    <html>
+        <head>
+            <title>Test Site</title>
+        </head>
+        <body>
+            <hr>
+            <h1>Input URLs here</h1>
+            <form action="" method="post" action="/results>
+                <label for="URL">Enter URLs:</label>
+                <textarea id="urltext" name="urltext" rows="5" cols="50"></textarea>
+                <br>
+                <input type="submit" value="submit">
+            </form>
+        </body>
+    </html>
+    """
+##
+#    if request.method == "POST":
+#        text = request.form['urltext']
+#        print(text)
+#        return render_template('results.html', title="TEST RESULT", urltext=text)
+#    return render_template('form.html', title='TEST URL')
+##
 
-
-@app.route("/results/{urltext}")
-async def demo_get(urltext):
+@app.post("/results", response_class=HTMLResponse)
+async def demo_get(request: Request):
     driver=createDriver()
 
     homepage = getGoogleHomepage(driver)
@@ -36,13 +54,30 @@ async def demo_get(urltext):
         results.append({url,chkSiteOk,chkReason})
     print(results)
     driver.close()
-    return render_template('results.html', title="TEST RESULT", results=results)
+    return """
+    <h1>Results</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>URL</th>
+                <th>Status</th>
+                <th>Reason</th>
+            </tr>
+        </thead>
+        <tbody>
+            {% for result in results %}
+            <tr>
+                <td>{{ result.url }}</td>
+                <td>{{ result.status }}</td>
+                <td>{{ result.reason }}</td>
+            </tr>
+            {% endfor %}
+        </tbody>
+    </table>
+    <br>
+    """
 
-@app.post("/backgroundDemo")
-async def demo_post(inp: Msg, background_tasks: BackgroundTasks):
-    
-    background_tasks.add_task(doBackgroundTask, inp)
-    return {"message": "Success, background task started"}
+
     
 
 
